c88d91c0e7dc9447fe76967e62985f938c719723
Administrator <Administrator@AUTOBVT-BB27FJP>
2018-10-16 16:38:52 +0800

    add

--- src/main/java/com/test/TestMain.java
+++ src/main/java/com/test/TestMain.java
@@ -1,9 +1,5 @@
 package com.test;
 
-/**
- * @author 柯博晓
- * @date 创建时间：2017年10月10日 下午3:53:30
- */
 
 public class TestMain {
 
@@ -14,6 +10,5 @@ public class TestMain {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Hello world!!");
 		System.out.println("This is git Push!!!");
-		System.out.println("This is git Pull!!!");
 	}
 }
